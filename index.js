258366fc42f3554713dda7899429e41f157e7b43
Sean Whitaker <47946594+swhitake81@users.noreply.github.com>
2022-01-13 21:11:46 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '797b0801',
+            s: searchTerm
+        }
+    });
+
+    if (response.data.Error) {
+        return [];
+    }
+    
+    return response.data.Search;
+};
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async e => {
+    const movies = await fetchData(e.target.value);
+
+    resultsWrapper.innerHTML = ''
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        const imgsrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src="${movie.imgsrc}" />
+            ${movie.Title}
+        `;
+
+        resultsWrapper.appendChild(option);
+    }
+};
+
+input.addEventListener('input', debounce(onInput));
